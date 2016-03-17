5f20253e1017ca293b863eb32c838f44fb6efb17
Jernej Kavka <jernejkavka@ssw.com.au>
2016-03-17 18:54:39 +0100

    Step - 1: - Add UI for LED indicator - Add button for switching On/Off - Add logic for switching UI LED

--- IotHackathonLedDemo/MainPage.xaml.cs
+++ IotHackathonLedDemo/MainPage.xaml.cs
@@ -1,19 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Windows.UI.Xaml.Shapes;
 
 namespace IotHackathonLedDemo
 {
@@ -22,9 +11,48 @@ namespace IotHackathonLedDemo
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private SolidColorBrush ledOn = new SolidColorBrush(Colors.Green);
+        private SolidColorBrush ledOff = new SolidColorBrush(Colors.Gray);
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            Loaded += PageLoaded;
+        }
+
+        private void PageLoaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            // Set to initial value
+            ToggleLed(1);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Toggle between on and off
+            ToggleLed(1);
+        }
+
+        /// <summary>
+        /// Helper method for switching LEDs
+        /// </summary>
+        /// <param name="ledId">LED ID</param>
+        private void ToggleLed(int ledId)
+        {
+            switch (ledId)
+            {
+                case 1:
+                    ToggleLed(LedIndicator1);
+                    break;
+            }
+        }
+        
+        private void ToggleLed(Ellipse rect)
+        {
+            var newState = rect.Fill == ledOn ? false : true;
+
+            // Change color of LED indicator.
+            rect.Fill = newState ? ledOn : ledOff;
         }
     }
 }
